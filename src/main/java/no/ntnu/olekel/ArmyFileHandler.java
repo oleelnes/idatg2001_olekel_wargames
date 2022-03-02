ff71e7fc8fd4c9ab34672dd46ab74a1991d8008f
Ole Kristian Elnæs <olekel@ntnu.no>
2022-03-02 13:42:36 +0100

    Added readArmyCSV method in ArmyFileHandler

--- src/main/java/no/ntnu/olekel/ArmyFileHandler.java
+++ src/main/java/no/ntnu/olekel/ArmyFileHandler.java
@@ -1,5 +1,6 @@
 package no.ntnu.olekel;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,6 +19,13 @@ import java.util.logging.Logger;
 public class ArmyFileHandler {
   static Logger logger = Logger.getLogger("logger");
 
+
+  /**
+   * This method writes information about an army to a csv file.
+   *
+   * @param army  The army whose information that will be written to file.
+   * @param path  The path of the csv file to which information will be written.
+   */
   public static void writeArmyCSV(Army army, Path path) {
     try (BufferedWriter writer = Files.newBufferedWriter(path)) {
       writer.write(army.getName());
@@ -38,6 +46,35 @@ public class ArmyFileHandler {
     }
   }
 
-  
+  /**
+   * This method reads information about an army in a CSV file,
+   * then creates an army with those specifications and returns
+   * that army.
+   *
+   * @param path  The path to the csv file to read
+   * @return      An instance of the class Army
+   */
+  public static Army readArmyCSV(Path path) {
+    Army armyFromFile;
 
+    try (BufferedReader reader = Files.newBufferedReader(path)) {
+      String lineOfText;
+      lineOfText = reader.readLine();
+      armyFromFile = new Army(lineOfText);
+      while ((lineOfText = reader.readLine()) != null) {
+        String[] words = lineOfText.split(",");
+        switch (words[0]) {
+          case "Infantry Unit" -> armyFromFile.add(new InfantryUnit(words[1], Integer.parseInt(words[2])));
+          case "Cavalry Unit" -> armyFromFile.add(new CavalryUnit(words[1], Integer.parseInt(words[2])));
+          case "Commander Unit" -> armyFromFile.add(new CommanderUnit(words[1], Integer.parseInt(words[2])));
+          case "Ranged Unit" -> armyFromFile.add(new RangedUnit(words[1], Integer.parseInt(words[2])));
+          default -> logger.log(Level.INFO, "Invalid type was attempted to be read");
+        }
+      }
+      return armyFromFile;
+    } catch (IOException e) {
+      logger.log(Level.WARNING, e.getMessage());
+      return null;
+    }
+  }
 }
