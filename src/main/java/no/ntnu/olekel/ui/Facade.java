a2979c5e39e07cc551ce9551f37b7470376da906
Ole Kristian Elnæs <olekel@ntnu.no>
2022-03-30 10:18:16 +0200

    Added the private constructor and getInstance methods in Facade.java

--- src/main/java/no/ntnu/olekel/ui/Facade.java
+++ src/main/java/no/ntnu/olekel/ui/Facade.java
@@ -1,4 +1,35 @@
 package no.ntnu.olekel.ui;
 
+/**
+ * Facade class that implements the Design Pattern Singleton.
+ * Holds objects that we only want one of.
+ *
+ * @author  Ole Kristian Elnæs
+ * @version 30.03.2022
+ */
 public class Facade {
+    private static volatile Facade instance;
+
+    /**
+     * Private constructor.
+     * todo: initialize fields.
+     */
+    private Facade(){
+
+    }
+
+    /**
+     * Method that checks whether an instance of the class exists or not,
+     * if not, it creates the instance. After this, it returns the instance.
+     *
+     * @return  The instance of the class.
+     */
+    public static Facade getInstance() {
+        if(instance == null) {
+            synchronized (Facade.class) {
+                instance = new Facade();
+            }
+        }
+        return instance;
+    }
 }
