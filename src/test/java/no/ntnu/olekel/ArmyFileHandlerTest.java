f450339f0fef23bab85bbe6db1b53bf8b5652b2c
Ole Kristian Elnæs <olekel@ntnu.no>
2022-03-30 11:50:50 +0200

    Tests for ArmyFileHandler.java

---
+++ src/test/java/no/ntnu/olekel/ArmyFileHandlerTest.java
@@ -0,0 +1,39 @@
+package no.ntnu.olekel;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArmyFileHandlerTest {
+    Army army;
+    List<Unit> units;
+
+    @BeforeEach
+    public void constructArmy(){
+        this.units = new ArrayList<Unit>();
+        this.units.add(new InfantryUnit("Infantry 1", 12));
+        this.units.add(new InfantryUnit("Infantry 2", 20));
+        this.units.add(new CavalryUnit("Cavalry 1", 10));
+        this.army = new Army("test", units);
+    }
+
+    @Test
+    public void readCSVTest() {
+        Army testArmy = ArmyFileHandler.readArmyCSV(Path.of("ArmyOne.csv"));
+        assertNotNull(testArmy);
+        assertEquals(testArmy.getAllUnits().get(0).getName(), "Infantry Unit 0");
+    }
+
+    @Test
+    public void writeCSVTest(){
+        ArmyFileHandler.writeArmyCSV(army, Path.of("test.csv"));
+        Army testArmy = ArmyFileHandler.readArmyCSV(Path.of("test.csv"));
+        assertNotNull(testArmy);
+    }
+
+}
