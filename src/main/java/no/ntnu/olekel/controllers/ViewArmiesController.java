479db8e4e4baef59a5917d10546d4180fa0a1ec0
Ole Kristian Elnæs <ole.elnaes@gmail.com>
2022-05-04 15:07:45 +0200

    First version of viewArmies.fxml is created

--- src/main/java/no/ntnu/olekel/controllers/ViewArmiesController.java
+++ src/main/java/no/ntnu/olekel/controllers/ViewArmiesController.java
@@ -1,4 +1,41 @@
 package no.ntnu.olekel.controllers;
 
-public class ViewArmiesController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import no.ntnu.olekel.core.units.Army;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewArmiesController implements Initializable {
+
+  @FXML
+  private TableView<Army> tableView;
+
+  @FXML
+  private TableColumn<Army, String> armyNameColumn;
+
+  @FXML
+  private TableColumn<Army, Integer> commanderUnitsColumn;
+
+  @FXML
+  private TableColumn<Army, Integer> rangedUnitsColumn;
+
+  @FXML
+  private TableColumn<Army, Integer> infantryUnitsColumn;
+
+  @FXML
+  private TableColumn<Army, Integer> cavalryUnitsColumn;
+
+  @FXML
+  private TableColumn<Army, Integer> healthColumn;
+
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+
+  }
+
 }
