5f5c3cbb86ca5fb9d3579941af640889ebad70dd
Ole Kristian Elnæs <ole.elnaes@gmail.com>
2022-05-05 18:36:00 +0200

    Improved createArmyPage.fxml.

--- src/main/java/no/ntnu/olekel/controllers/CreateArmyController.java
+++ src/main/java/no/ntnu/olekel/controllers/CreateArmyController.java
@@ -8,9 +8,21 @@ import no.ntnu.olekel.ui.Scenes;
 
 import java.io.IOException;
 
+/**
+ *
+ *
+ * @version {@value no.ntnu.olekel.constants.Constants#VERSION}
+ * @author  Ole Kristian Elnæs
+ */
 public class CreateArmyController {
   Scenes scenes = Facade.getInstance().getScenes();
 
+  /**
+   * Method that loads the main page through the loadScene() method in Scenes.
+   *
+   * @param event
+   * @throws IOException
+   */
   @FXML
   public void mainPageAction(ActionEvent event) throws IOException {
     scenes.loadScene(event, ClassPaths.mainPageURL);
