0e6f75c27350a5a481768b0930b2f0ff0bec9bdf
Ole Kristian Elnæs <ole.elnaes@gmail.com>
2022-05-20 18:23:35 +0200

    Added javadocs to CreateArmyController.java and MainPageController.java

--- src/main/java/no/ntnu/olekel/controllers/CreateArmyController.java
+++ src/main/java/no/ntnu/olekel/controllers/CreateArmyController.java
@@ -103,6 +103,33 @@ public class CreateArmyController implements Initializable {
   @FXML
   private ListView<Text> existingUnitsListView;
 
+  /**
+   * This method initializes the necessary fields and other content
+   * when the createArmyPage.fxml is loaded.
+   *
+   * @param url             url
+   * @param resourceBundle  url
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    setAddUnitsContentVisibility(false);
+    this.armyName = ""; //this might work but take a closer look at it.
+    this.units = new ArrayList<>();
+    this.state = State.NEW;
+    this.listContent = new ArrayList<>();
+    this.newArmy = new Army("");
+
+    ObservableList<Text> stringObservableList = FXCollections.observableArrayList(listContent);
+    existingUnitsListView.setItems(stringObservableList);
+  }
+
+  /**
+   * Method that opens a filechooser for the user to select a file from, from which
+   * units will be imported into the existing army file, given that the content of the file
+   * is in line with the requirements for an army file.
+   *
+   * @param event When "import units from file" button is clicked.
+   */
   @FXML
   public void loadFromFileAction(ActionEvent event) {
     if (!army.getAllUnits().isEmpty()) {
@@ -131,58 +158,84 @@ public class CreateArmyController implements Initializable {
 
   }
 
+  /**
+   * The state of the army that is being created. It is "NEW" if it has not been
+   * added to the register yet; it is "EDIT" if it has.
+   */
   public enum State {
     NEW,
     EDIT;
   }
 
-  @Override
-  public void initialize(URL url, ResourceBundle resourceBundle) {
-    setAddUnitsContentVisibility(false);
-    this.armyName = ""; //this might work but take a closer look at it.
-    this.units = new ArrayList<>();
-    this.state = State.NEW;
-    this.listContent = new ArrayList<>();
-    this.newArmy = new Army("");
-
-    ObservableList<Text> stringObservableList = FXCollections.observableArrayList(listContent);
-    existingUnitsListView.setItems(stringObservableList);
-    //updateListContent();
-  }
-
+  /**
+   * Method that adds units to the army being created according to user input.
+   *
+   * @param event When the "add" button for commander units has been pressed.
+   */
   @FXML
   public void addCommanderUnitsAction(ActionEvent event) {
     addUnits(UnitFactory.Type.COMMANDER, Integer.parseInt(commanderUnitsAmountInput.getText()),
         Integer.parseInt(commanderUnitsHealthInput.getText()));
   }
 
+  /**
+   * Method that adds units to the army being created according to user input.
+   *
+   * @param event When the "add" button for infantry units has been pressed.
+   */
   @FXML
   public void addInfantryUnitsAction(ActionEvent event) {
     addUnits(UnitFactory.Type.INFANTRY, Integer.parseInt(infantryUnitsAmountInput.getText()),
         Integer.parseInt(infantryUnitsHealthInput.getText()));
   }
 
+  /**
+   * Method that adds units to the army being created according to user input.
+   *
+   * @param event When the "add" button for ranged units has been pressed.
+   */
   @FXML
   public void addRangedUnitsAction(ActionEvent event) {
     addUnits(UnitFactory.Type.RANGED, Integer.parseInt(rangedUnitsAmountInput.getText()),
         Integer.parseInt(rangedUnitsHealthInput.getText()));
   }
 
+  /**
+   * Method that adds units to the army being created according to user input.
+   *
+   * @param event When the "add" button for cavalry units has been pressed.
+   */
   @FXML
   public void addCavalryUnitsAction(ActionEvent event) {
     addUnits(UnitFactory.Type.CAVALRY, Integer.parseInt(cavalryUnitsAmountInput.getText()),
         Integer.parseInt(cavalryUnitsHealthInput.getText()));
   }
 
+  /**
+   * TODO:
+   * @return true or false depending on whether the input is valid.
+   */
   private boolean checkDigit(){
     return true;
   }
 
+  /**
+   * This method adds units to the army that is being created, depending on
+   * parameters that is passed after being inputted by the user.
+   *
+   * @param unitType  The type of unit (Commander, infantry, etc.)
+   * @param amount    The amount of units to add.
+   * @param health    The health of the units to add.
+   */
   private void addUnits(UnitFactory.Type unitType, int amount, int health){
     newArmy.addUnitList(Facade.getInstance().getUnitFactory().createUnitList(unitType, "name", health, amount));
     updateListContent();
   }
 
+  /**
+   * This method updates the contents of the existingUnitsListView ListView, which displays the
+   * currently existing units in the army that is being created.
+   */
   private void updateListContent(){
     listContent.clear();
     if(state == State.EDIT) setListContent(army);
@@ -191,6 +244,13 @@ public class CreateArmyController implements Initializable {
     existingUnitsListView.setItems(stringObservableList);
   }
 
+  /**
+   * This method updates the listContent ArrayList which contain all the units that exist in the
+   * army that is being created. The listContent ArrayList is being used by the
+   * existingUnitsListView ListView to display the currently existing units.
+   *
+   * @param listArmy  Which army to fetch units from.
+   */
   private void setListContent(Army listArmy){
     for (int i = 0; i < listArmy.getAllUnits().size(); i++) {
       Text text = new Text(listArmy.getAllUnits().get(i).getType() + ": " + listArmy.getAllUnits().get(i).toString());
@@ -202,15 +262,21 @@ public class CreateArmyController implements Initializable {
   /**
    * Method that loads the main page through the loadScene() method in Scenes.
    *
-   * @param event
-   * @throws IOException
+   * @param event         When back to main page button is pressed.
+   * @throws IOException  Input/output exception.
    */
   @FXML
   public void mainPageAction(ActionEvent event) throws IOException {
     scenes.loadScene(event, ClassPaths.mainPageURL);
   }
 
-
+  /**
+   * This method sets the visibility of the buttons that are part of
+   * "add units to [army name]"
+   *
+   * @param visibility  Boolean variable that decides whether the buttons that are part of
+   *                    "add units to [army name]" should be visible.
+   */
   private void setAddUnitsContentVisibility(boolean visibility){
     commanderUnitsAmountInput.setVisible(visibility);
     cavalryUnitsAmountInput.setVisible(visibility);
@@ -266,15 +332,24 @@ public class CreateArmyController implements Initializable {
     }
   }
 
+  /**
+   * This method saves the army that is being created/edited to the army register.
+   *
+   * @param event When "save army" button is pressed.
+   */
   @FXML
   public void saveArmyAction(ActionEvent event) {
     switch(state) {
       case NEW -> newArmy();
       case EDIT -> editArmy();
-      default -> System.err.println("not valid input");
+      default -> System.err.println("not valid input"); //todo: logger
     }
   }
 
+  /**
+   * This method saves an army that has not yet been saved to the
+   * facade (ui/Facade.java) to the facade and the army register.
+   */
   private void newArmy(){
     if (!armyName.equals("")) {
       newArmy.setName(armyName);
@@ -292,6 +367,10 @@ public class CreateArmyController implements Initializable {
     // in viewArmies, when edit is pressed, also set Facade army to be the selected army.
   }
 
+  /**
+   * This method saves an army that already is stored as the army in the facade (ui/Facade.java)
+   * to the army register.
+   */
   private void editArmy(){
     if (Facade.getInstance().getArmyRegister().getArmyRegister().stream()
         .map(a -> a.getName().toLowerCase(Locale.ROOT))
@@ -302,16 +381,25 @@ public class CreateArmyController implements Initializable {
     Facade.getInstance().getArmyRegister().getArmyRegister().add(army);
   }
 
+  /**
+   * This method changes the state of the army --> whether or not it has been stored as the army instance
+   * in the facade (ui/Facade.java).
+   *
+   * @param state The state of the army being created ("NEW" or "EDIT")
+   */
   public void setState(State state) {
     this.state = state;
     updateName();
   }
 
+  /**
+   * This method updates the name of the army being created.
+   */
   private void updateName(){
     switch (state) {
       case NEW -> armyName = "";
       case EDIT -> armyName = Facade.getInstance().getArmy().getName();
-      default -> System.err.println("not valid input");
+      default -> System.err.println("not valid input"); //todo: logger.
     }
   }
 
--- src/main/java/no/ntnu/olekel/controllers/MainPageController.java
+++ src/main/java/no/ntnu/olekel/controllers/MainPageController.java
@@ -18,26 +18,60 @@ public class MainPageController {
 
   private final Scenes scenes = Facade.getInstance().getScenes();
 
+  /**
+   * This method is triggered when the "new war" button is pressed, and changes
+   * the displayed scene to the new war page.
+   *
+   * @param event         Actionevent: when new war button is clicked
+   * @throws IOException  Input/output exception
+   */
   @FXML
   private void newWarAction(ActionEvent event) throws IOException {
     scenes.loadScene(event, ClassPaths.newWarPageURL);
   }
 
+  /**
+   * This method is triggered when the "view armies" button is pressed, and changes
+   * the displayed scene to the view armies page.
+   *
+   * @param event         Actionevent: when view armies button is clicked
+   * @throws IOException  Input/output exception
+   */
   @FXML
   public void viewArmies(ActionEvent event) throws IOException {
     scenes.loadScene(event, ClassPaths.viewArmiesURL);
   }
 
+  /**
+   * This method is triggered when the "new army" button is pressed, and changes
+   * the displayed scene to the new army page.
+   *
+   * @param event         Actionevent: when new war button is clicked
+   * @throws IOException  Input/output exception
+   */
   @FXML
   public void newArmyAction(ActionEvent event) throws IOException {
     scenes.loadScene(event, ClassPaths.createArmyPageURL);
   }
 
+  /**
+   * todo:
+   *
+   * @param event         Actionevent: when main page button is clicked
+   * @throws IOException  Input/output exception
+   */
   @FXML
   public void simpleWarSimulationAction(ActionEvent event) throws IOException {
     scenes.loadScene(event, ClassPaths.simpleWarSimulationPageURL);
   }
 
+  /**
+   * This method is triggered when the "add army from file" button is pressed, and changes
+   * the displayed scene to the add army from file page.
+   *
+   * @param event         Actionevent: when add army from file button is clicked
+   * @throws IOException  Input/output exception
+   */
   @FXML
   public void addArmyFromFileAction(ActionEvent event) throws IOException {
     Facade.getInstance().getDialogsHandler().addArmyFromFileDialog(event);
